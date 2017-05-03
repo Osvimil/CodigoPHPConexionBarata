806bb8844f9d197569ccefd68e6da7f49e4999cb
oswaldoadidas <oswaldoadidas@hotmail.com>
2017-05-03 13:27:11 -0500

    adding another example inside the same example

--- webservice.php
+++ webservice.php
@@ -14,4 +14,25 @@ $response = curl_exec($handle);
 $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 curl_close($handle);
 print_r($response);
+
+/* --------------------------------------------------------------------------------------------------------------*/
+$usuarioWebService= 'cepsistemas'; //Nombre_usuario webService
+$passWebService = 'ZOpU7MFr8vXV7OE6c8cMui71IIYDESFaZ9FAjml8'; //Contrasena WebService
+$url_webService = 'https://www.saep.unam.mx/api-2/cep/alumno/'.$num_cuenta; // URL del webService.
+$ch=curl_init($url_webService);
+                    $options=array(CURLOPT_SSL_VERIFYPEER => false,
+                    			   CURLOPT_RETURNTRANSFER => false,
+                    			   CURLOPT_USERPWD        => "{$usuarioWebService}:{$passWebService}",
+        						   CURLOPT_HTTPHEADER     => array( "Accept: application/json" ),
+    				);
+    				curl_setopt_array( $ch, $options );
+					$result = curl_exec( $ch );
+					$info = curl_getinfo($ch);
+					curl_close($ch);
+
+					$data = json_encode($result);
+
+					print_r($result);					
+echo '<br><br>';
+exit;
 ?>
